7d984392f9b24407e9080945eb4c19f75c28f18b
sb141 <sankit.sb.chandel@gmail.com>
2020-09-19 05:55:44 +0530

    Added product and category business entities and created database. enabled the migration.

---
+++ MvcComDatabase/MvcComContext.cs
@@ -0,0 +1,22 @@
+﻿using MvcComEntities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcComDatabase
+{
+    public class MvcComContext : DbContext
+    {
+
+        public MvcComContext() : base("MvcComConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ MvcComEntities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcComEntities
+{
+    public class Product:BaseEntity
+    {
+        public Category category { get; set; }
+        public decimal Price { get; set; }
+        
+    }
+}
