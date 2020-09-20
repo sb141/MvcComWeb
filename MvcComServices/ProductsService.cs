16367bb15c5b35a4d93413f287fd3c58da815304
sb141 <sankit.sb.chandel@gmail.com>
2020-09-21 03:11:46 +0530

    Added CRUD functionlaity for the products page. Added a theme for the site and created a home page.

---
+++ MvcComServices/ProductsService.cs
@@ -0,0 +1,60 @@
+﻿using MvcComDatabase;
+using MvcComEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcComServices
+{
+    public class ProductsService
+    {
+        public List<Product> GetProducts()
+        {
+            using (var context = new MvcComContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        public Product GetProduct(int ID)
+        {
+            using (var context = new MvcComContext())
+            {
+                return context.Products.Find(ID);
+            }
+        }
+
+        public void SaveProduct(Product product)
+        {
+            using(var context = new MvcComContext())
+            {
+                context.Products.Add(product);
+
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new MvcComContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteProduct(int ID)
+        {
+            using (var context = new MvcComContext())
+            {
+                var product = context.Products.Find(ID);
+                //context.Entry(category).State = System.Data.Entity.EntityState.Deleted;
+                //both will work
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ MvcComWeb/Controllers/ProductController.cs
@@ -0,0 +1,73 @@
+﻿using MvcComEntities;
+using MvcComServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcComWeb.Controllers
+{
+        public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductsTable(string search)
+        {
+            var products = productsService.GetProducts();
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                //products = products.Where(p => p.Name == search).ToList();
+
+                //or
+                products = products.Where(p => p.Name != null && p.Name.ToLower().Contains(search.ToLower())).ToList();
+
+            }
+
+            return PartialView(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productsService.SaveProduct(product);
+            return RedirectToAction("ProductsTable");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var product = productsService.GetProduct(ID);
+            return PartialView(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            productsService.UpdateProduct(product);
+            return RedirectToAction("ProductsTable");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int ID)
+        {
+            productsService.DeleteProduct(ID);
+            return RedirectToAction("ProductsTable");
+        }
+
+
+    }
+}
